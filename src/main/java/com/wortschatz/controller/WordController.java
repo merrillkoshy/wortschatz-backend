d6c4a2817ad1e55cefca077cdfcd2087484b2fc1
Merrill Thomas <merrillthomas@Merrills-MacBook-Pro.local>
2025-10-07 13:20:45 +0530

    local connect with psql

--- src/main/java/com/wortschatz/controller/WordController.java
+++ src/main/java/com/wortschatz/controller/WordController.java
@@ -2,6 +2,7 @@ package com.wortschatz.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import com.wortschatz.repository.WordRepository;
 
 @RestController
 @RequestMapping("/api/words")
+@CrossOrigin(origins = "http://localhost:5173")
 public class WordController {
     private final WordRepository wordRepository;
 
